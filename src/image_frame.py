e3d07bfdf48462646d9c03ff83ecc9c3567a511c
Anish Zute <31576491+anishzute@users.noreply.github.com>
2023-08-12 03:29:27 -0500

    Added more comments

--- src/image_frame.py
+++ src/image_frame.py
@@ -3,64 +3,72 @@ from image_generator import generate_cells_image, generate_cells_plot
 from image_segmentation import segment_cells_plot
 from PIL import Image
 
+
+# ImageFrame class to display generated and segmented cell images
 class ImageFrame(ctk.CTkFrame):
-	def __init__(self, master, shared_vars, *args, **kwargs):
-		super().__init__(master, *args, **kwargs)
-		self.shared_vars = shared_vars
-		# self.title = "Images"
-		# self.title = ctk.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
-		# self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew",columnspan=3)
+    def __init__(self, master, shared_vars, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.shared_vars = shared_vars
+        
+        # Configure grid weights for proper scaling
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
 
-		
-		self.grid_columnconfigure((0,1), weight = 1)
-		self.grid_rowconfigure((0,1,2), weight = 1)
+        # Label for generated images section
+        self.generate_label = ctk.CTkLabel(self, text="Generated Image", fg_color="gray30", corner_radius=6)
+        self.generate_label.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="new")
 
-		#Generated Images
-		self.generate_label = ctk.CTkLabel(self, text="Generated Image", fg_color="gray30", corner_radius=6)
-		self.generate_label.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="new")
+        # Label to display the generated image
+        self.generated_image_label = ctk.CTkLabel(self, text="")
+        self.generated_image_label.grid(row=1, column=0, padx=.5, pady=(10, 0), sticky="news")
 
-		self.generated_image_label = ctk.CTkLabel(self, text="")  
-		self.generated_image_label.grid(row=1, column=0, padx=.5, pady=(10, 0), sticky="news")
+        # Button to trigger image generation
+        self.generate_button = ctk.CTkButton(self, text="Generate", command=self.generateButton)
+        self.generate_button.grid(row=3, column=0, padx=10, pady=10)
 
+        # Label for segmented images section
+        self.segment_label = ctk.CTkLabel(self, text="Segmented Image", fg_color="gray30", corner_radius=6)
+        self.segment_label.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="new")
 
-		self.generate_button = ctk.CTkButton(self, text="Generate", command=self.generateButton)
-		self.generate_button.grid(row=3, column=0, padx=10, pady=10)
+        # Label to display the segmented image
+        self.segmented_image_label = ctk.CTkLabel(self, text="")
+        self.segmented_image_label.grid(row=1, column=1, padx=0.5, pady=(10, 0), sticky="news")
 
-		#Segment Images
-		self.segment_label = ctk.CTkLabel(self, text="Segmented Image", fg_color="gray30", corner_radius=6)
-		self.segment_label.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="new")
+        # Button to trigger image segmentation
+        self.generate_button = ctk.CTkButton(self, text="Segment", command=self.segmentButton)
+        self.generate_button.grid(row=3, column=1, padx=10, pady=10)
 
-		self.segmented_image_label = ctk.CTkLabel(self, text="")  
-		self.segmented_image_label.grid(row=1, column=1, padx=0.5, pady=(10, 0), sticky="news")
+    # Function to generate a cell image based on shared variables
+    def generateButton(self):
+        # Code to generate the image and set the desired width, aspect ratio, and resizing
+        # Configure the generated image label with the new image
+        shared_vars = self.shared_vars
+        shared_vars.color_image = generate_cells_image(shared_vars.width.get(), shared_vars.height.get(), shared_vars.num_cells.get(), shared_vars.min_size.get(), shared_vars.max_size.get(), shared_vars.min_intensity.get(), shared_vars.max_intensity.get(), shared_vars.noise_mean.get(), shared_vars.noise_dev.get(), blur_amount=7)
+        shared_vars.color_plot = generate_cells_plot(shared_vars.color_image)
 
-		self.generate_button = ctk.CTkButton(self, text="Segment", command=self.segmentButton)
-		self.generate_button.grid(row=3, column=1, padx=10, pady=10)
-	
-	def generateButton(self):
-		shared_vars = self.shared_vars
-		shared_vars.color_image = generate_cells_image(shared_vars.width.get(), shared_vars.height.get(), shared_vars.num_cells.get(), shared_vars.min_size.get(), shared_vars.max_size.get(), shared_vars.min_intensity.get(), shared_vars.max_intensity.get(), shared_vars.noise_mean.get(), shared_vars.noise_dev.get(), blur_amount=7)
-		shared_vars.color_plot = generate_cells_plot(shared_vars.color_image)
+        desired_width = 480
+        original_width, original_height = shared_vars.color_plot.size
+        aspect_ratio = original_height / original_width
+        new_height = int(desired_width * aspect_ratio)
+        # resized_image = shared_vars.color_plot.image.resize((desired_width, new_height), Image.LANCZOS)
 
-		desired_width = 480
-		original_width, original_height = shared_vars.color_plot.size
-		aspect_ratio = original_height / original_width
-		new_height = int(desired_width * aspect_ratio)
-		# resized_image = shared_vars.color_plot.image.resize((desired_width, new_height), Image.LANCZOS)
+        generated_image = ctk.CTkImage(dark_image=shared_vars.color_plot, size=(desired_width, new_height))
+        self.generated_image_label.configure(image=generated_image)
+        return generated_image
+    
+    # Function to segment a cell image based on shared variables
+    def segmentButton(self):
+        # Code to segment the image and set the desired width, aspect ratio, and resizing
+        # Configure the segmented image label with the new image
+        shared_vars = self.shared_vars
+        shared_vars.segmented_plot = segment_cells_plot(shared_vars.color_image, shared_vars.kernel_size.get(), shared_vars.min_distance.get())
 
-		generated_image = ctk.CTkImage(dark_image=shared_vars.color_plot, size=(desired_width, new_height))
-		self.generated_image_label.configure(image=generated_image)
-		return generated_image
+        desired_width = 480
+        original_width, original_height = shared_vars.color_plot.size
+        aspect_ratio = original_height / original_width
+        new_height = int(desired_width * aspect_ratio)
+        # resized_image = shared_vars.color_plot.image.resize((desired_width, new_height), Image.LANCZOS)
 
-	def segmentButton(self):
-		shared_vars = self.shared_vars
-		shared_vars.segmented_plot = segment_cells_plot(shared_vars.color_image, shared_vars.kernel_size.get(), shared_vars.min_distance.get())
-		
-		desired_width = 480
-		original_width, original_height = shared_vars.color_plot.size
-		aspect_ratio = original_height / original_width
-		new_height = int(desired_width * aspect_ratio)
-		# resized_image = shared_vars.color_plot.image.resize((desired_width, new_height), Image.LANCZOS)
-
-		segmented_image = ctk.CTkImage(dark_image=shared_vars.segmented_plot, size=(desired_width, new_height))
-		self.segmented_image_label.configure(image=segmented_image)
-		return segmented_image
+        segmented_image = ctk.CTkImage(dark_image=shared_vars.segmented_plot, size=(desired_width, new_height))
+        self.segmented_image_label.configure(image=segmented_image)
+        return segmented_image
--- src/main.py
+++ src/main.py
@@ -7,26 +7,38 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
+        # Set the title and geometry of the main application window
         self.title("Synthetic Cell Image Generation and Segmentation")
         self.geometry('1600x720')
 
+        # Configure grid layout for the main window
         self.grid_columnconfigure(0, weight=0)
         self.grid_columnconfigure((1, 2), weight=1)
         self.grid_rowconfigure((0, 1), weight=1)
 
+        # Create an instance of SharedVariables to hold shared data
         shared_vars = SharedVariables()
 
+        # Create and position the GenerateSliderFrame
+        # This frame contains sliders to control cell image generation parameters
         self.generate_slider_frame = GenerateSliderFrame(self, shared_vars)
         self.generate_slider_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nesw")
 
+        # Create and position the SegmentSliderFrame
+        # This frame contains sliders to control cell image segmentation parameters
         self.segment_slider_frame = SegmentSliderFrame(self, shared_vars)
         self.segment_slider_frame.grid(row=1, column=0, padx=10, pady=(10, 10), sticky="nesw")
 
+        # Create and position the ImageFrame
+        # This frame displays the generated and segmented cell images
         self.image_frame = ImageFrame(self, shared_vars)
         self.image_frame.grid(row=0, column=1, padx=10, pady=(10, 10), sticky="nesw", columnspan=2, rowspan=2)
 
-
+# Set the appearance mode and color theme for customtkinter
 ctk.set_appearance_mode("System")  # Modes: system (default), light, dark
 ctk.set_default_color_theme("blue")
+
+# Create an instance of the App class and start the main loop
 app = App()
 app.mainloop()
+
